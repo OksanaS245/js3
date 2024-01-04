2b26ea16f5041f147986fd280e1115cdec211295
OksanaS245 <ksenyland@gmail.com>
2024-01-04 18:41:38 +0000

    add js3.js

---
+++ js3.js
@@ -0,0 +1,73 @@
+//task 1
+let i = 30;
+while (i>=30 && i<40) {
+    i++;
+    if (i%2 == 0) continue;
+    console.log (i)
+
+}
+
+//task 2
+for (let i = 70; i<=70 && i>=40; i--) {
+    if (i%2 ==1) continue;
+    console.log (i);
+}
+
+//task 3
+let x = 71;
+while (x <=71 && x>40 ) {
+    x--;
+    if (x%2 ==1) continue;
+    console.log (x)
+    
+}
+
+//task 4
+let summ = 0;
+let number=0;
+while (number <100) {
+    number++
+    summ+=number};
+console.log (summ);
+
+//task 5
+let numb = 1;
+while (numb <=10) {
+let r = ':)'.repeat (numb);
+console.log (r);
+numb++;
+};
+
+//task 6
+let number2 = 0;
+while (number2 <50) {
+    let a = 'Java';
+    let b = 'Script'
+    number2++
+    if (number2 % 3 == 0 && number2 % 5 == 0) {console.log (a+b)}
+    else if (number2%3 == 0) {console.log (a)}
+    else if (number2%5 == 0) {console.log (b)}
+    else {console.log (number2)}
+};
+
+//task 7
+let k = 1000;
+let it = 0;
+
+while (k>50) {
+    k/=2;
+    it++;
+    }
+    console.log (it);
+
+//task 8 
+let m = 44;
+while (m>=44 && m<170) {
+    m++;
+    if (m%10 != 0)continue;
+    console.log (m);
+    }
+    
+
+
+
